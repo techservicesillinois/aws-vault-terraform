5f94d98e7a37d7f7ef591bdb1ff35054dfd27bef
Stephen J. Butler <stephen.butler@gmail.com>
2018-06-28 18:50:04 -0500

    Add secret for master key

--- terraform/_outputs.tf
+++ terraform/_outputs.tf
@@ -2,6 +2,10 @@ output "vault_key_id" {
     value = "${aws_kms_key.vault.id}"
 }
 
+output "vault_master_arn" {
+    value = "${aws_secretsmanager_secret.vault_master.arn}"
+}
+
 output "vault_storage_name" {
     value = "${aws_dynamodb_table.vault_storage.name}"
 }
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -24,3 +24,22 @@ resource "aws_kms_alias" "vault" {
     name = "alias/${var.project}"
     target_key_id = "${aws_kms_key.vault.id}"
 }
+
+
+resource "aws_secretsmanager_secret" "vault_master" {
+    name = "${var.project}/master"
+    description = "Segmented master keys for unsealing vault."
+
+    kms_key_id = "${aws_kms_key.vault.id}"
+    recovery_window_in_days = 7
+
+    tags {
+        Service = "${var.service}"
+        Contact = "${var.contact}"
+        DataClassification = "Sensitive"
+        Environment = "${var.environment}"
+
+        Project = "${var.project}"
+        NetID = "${var.contact}"
+    }
+}
