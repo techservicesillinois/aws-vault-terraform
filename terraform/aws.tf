658fa541652b8ed2392f1f4882e6cd4bc6c576dd
Stephen J. Butler <stephen.butler@gmail.com>
2018-06-28 21:19:44 -0500

    Add some useful AWS shared values

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -2,6 +2,24 @@
 # Data
 # ===================================================================
 
+data "aws_caller_identity" "current" {}
+
+
+data "aws_iam_policy_document" "instance_assume_role" {
+    statement {
+        effect = "Allow"
+        actions = [ "sts:AssumeRole" ]
+        principals {
+            type = "Service"
+            identifiers = [ "ec2.amazonaws.com" ]
+        }
+    }
+}
+
+
 data "aws_iam_role" "appautoscaling_dynamodb" {
     name = "AWSServiceRoleForApplicationAutoScaling_DynamoDBTable"
 }
+
+
+data "aws_region" "current" {}
